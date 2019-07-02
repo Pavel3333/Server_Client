6c9830f93c1e2ad1daa7284b7cfefc9d2ed5552f
Павел <pavel.ushaev2012@yandex.ru>
2019-07-02 20:58:03 +0300

    Рефакторинг, переход на класс

--- Server_Client/main.cpp
+++ Server_Client/main.cpp
@@ -1,110 +1,127 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
-
-#define WIN32_LEAN_AND_MEAN
-
-#include <winsock2.h>
-#include <Ws2tcpip.h>
-#include <stdio.h>
+#include "main.h"
 
 // Link with ws2_32.lib
 #pragma comment(lib, "Ws2_32.lib")
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT 27015
+Client::Client() {
+	state = CLIENT_STATE::SUCCESS;
 
-int main() {
+	ConnectSocket = INVALID_SOCKET;
+}
 
-	//----------------------
-	// Declare and initialize variables.
-	int iResult;
-	WSADATA wsaData;
+Client::~Client() {
+	if(state > INIT_WINSOCK) error_code = WSAGetLastError();
 
-	SOCKET ConnectSocket = INVALID_SOCKET;
-	struct sockaddr_in clientService;
+	// Вывод сообщения об ошибке
 
-	int recvbuflen = DEFAULT_BUFLEN;
-	const char *sendbuf = "Client: sending data test";
-	char recvbuf[DEFAULT_BUFLEN] = "";
+	const char* error_msg = nullptr;
 
-	//----------------------
+	switch (state)
+	{
+	case INIT_WINSOCK:
+		error_msg = "INIT_WINSOCK - error: %d\n";
+		break;
+	case CREATE_SOCKET:
+		error_msg = "CREATE_SOCKET - error: %ld\n";
+		break;
+	case CONNECT:
+		error_msg = "CONNECT - error: %d\n";
+		break;
+	case SEND:
+		error_msg = "SEND - error: %d\n";
+		break;
+	case SHUTDOWN:
+		error_msg = "SHUTDOWN - error: %d\n";
+		break;
+	case RECEIVE:
+		error_msg = "RECEIVE - error: %d\n";
+		break;
+	case CLOSE_SOCKET:
+		error_msg = "CLOSE_SOCKET - error: %d\n";
+		break;
+	default: break;
+	}
+
+	if(error_msg) printf(error_msg, error_code);
+
+	if(state > CREATE_SOCKET) closesocket(ConnectSocket);
+	if(state > INIT_WINSOCK)  WSACleanup();
+}
+
+bool Client::connect2Server() {
 	// Initialize Winsock
-	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (iResult != NO_ERROR) {
-		wprintf(L"WSAStartup failed with error: %d\n", iResult);
-		return 1;
-	}
+	state = CLIENT_STATE::INIT_WINSOCK;
 
-	//----------------------
-	// Create a SOCKET for connecting to server
-	ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (ConnectSocket == INVALID_SOCKET) {
-		wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
-		WSACleanup();
-		return 1;
-	}
+	if (error_code = WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) return true;
 
-	//----------------------
+	// Create a SOCKET for connecting to server (TCP/IP protocol)
+	state = CLIENT_STATE::CREATE_SOCKET;
+
+	ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+	if (ConnectSocket == INVALID_SOCKET) return true;
+
 	// The sockaddr_in structure specifies the address family,
 	// IP address, and port of the server to be connected to.
+
 	clientService.sin_family = AF_INET;
-	inet_pton(AF_INET, "127.0.0.1", &clientService.sin_addr.s_addr);
-	clientService.sin_port = htons(DEFAULT_PORT);
+	clientService.sin_port   = htons(DEFAULT_PORT);
+	inet_pton(AF_INET, SERVER_IP, &clientService.sin_addr.s_addr);
 
-	//----------------------
 	// Connect to server.
-	iResult = connect(ConnectSocket, (SOCKADDR*)&clientService, sizeof(clientService));
-	if (iResult == SOCKET_ERROR) {
-		wprintf(L"connect failed with error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
+	state = CLIENT_STATE::CONNECT;
 
-	//----------------------
+	if (connect(ConnectSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) return true;
+
+	return state = CLIENT_STATE::SUCCESS;
+}
+
+bool Client::sendData(const char* data, int size) {
 	// Send an initial buffer
-	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-	if (iResult == SOCKET_ERROR) {
-		wprintf(L"send failed with error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
+	state = CLIENT_STATE::SEND;
 
-	printf("Bytes Sent: %d\n", iResult);
+	bytesSent = send(ConnectSocket, data, size, 0);
+	if (bytesSent == SOCKET_ERROR) return true;
 
+	printf("Bytes Sent: %d\n", bytesSent);
+
+	return state = CLIENT_STATE::SUCCESS;
+}
+
+bool Client::receiveData(char* data, int size) {
 	// shutdown the connection since no more data will be sent
-	iResult = shutdown(ConnectSocket, SD_SEND);
-	if (iResult == SOCKET_ERROR) {
-		wprintf(L"shutdown failed with error: %d\n", WSAGetLastError());
-		closesocket(ConnectSocket);
-		WSACleanup();
-		return 1;
-	}
+	state = CLIENT_STATE::SHUTDOWN;
+
+	if (shutdown(ConnectSocket, SD_SEND) == SOCKET_ERROR) return true;
 
 	// Receive until the peer closes the connection
+	state = CLIENT_STATE::RECEIVE;
+
 	do {
+		bytesRec = recv(ConnectSocket, data, size, 0);
+		if (bytesRec > 0)       printf("Bytes received: %d\n", bytesRec);
+		else if (bytesRec == 0) printf("Connection closed\n");
+		else return true;
+	} while (bytesRec > 0);
 
-		iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
-		if (iResult > 0)
-			wprintf(L"Bytes received: %d\n", iResult);
-		else if (iResult == 0)
-			wprintf(L"Connection closed\n");
-		else
-			wprintf(L"recv failed with error: %d\n", WSAGetLastError());
+	return state = CLIENT_STATE::SUCCESS;
+}
 
-	} while (iResult > 0);
-
-
+bool Client::disconnect() {
 	// close the socket
-	iResult = closesocket(ConnectSocket);
-	if (iResult == SOCKET_ERROR) {
-		wprintf(L"close failed with error: %d\n", WSAGetLastError());
-		WSACleanup();
-		return 1;
+	state = CLIENT_STATE::CLOSE_SOCKET;
+
+	if (closesocket(ConnectSocket) == SOCKET_ERROR) return true;
+
+	return state = CLIENT_STATE::SUCCESS;
+}
+
+int main() {
+	Client* client = new Client();
+
+	if (client->connect2Server()) {
+		delete client;
+		client = nullptr;
 	}
 
-	WSACleanup();
 	return 0;
 }
---
+++ Server_Client/main.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+
+#include <iostream>
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT   27015
+#define SERVER_IP      "127.0.0.1"
+
+enum [[nodiscard]] ERROR_TYPE : uint8_t {
+	OK = 0,        // Без ошибок
+	WARNING,       // Предупреждение
+	SOFT_ERROR,    // Ошибка
+	CRITICAL_ERROR // Критическая ошибка
+};
+
+enum [[nodiscard]] CLIENT_STATE : uint8_t {
+	SUCCESS = 0,
+	INIT_WINSOCK,
+	CREATE_SOCKET,
+	CONNECT,
+	SEND,
+	SHUTDOWN,
+	RECEIVE,
+	CLOSE_SOCKET
+};
+
+class Client {
+public:
+	Client();
+	~Client();
+
+	int error_code;
+
+	CLIENT_STATE state;
+
+	int bytesSent;
+	int bytesRec;
+
+	bool connect2Server();
+	bool sendData(const char*, int);
+	bool receiveData(char*, int);
+	bool disconnect();
+private:
+	WSADATA wsaData;
+
+	SOCKET ConnectSocket;
+
+	struct sockaddr_in clientService;
+};
