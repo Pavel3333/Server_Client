a351c8979a9d3be37bf667d55bba434c3ce53dfe
Simi4 <you@example.com>
2019-12-08 01:09:20 +0300

    Fix codestyle in Socket

--- Common/Socket.cpp
+++ Common/Socket.cpp
@@ -1,18 +1,21 @@
-#pragma once
 #include "Socket.h"
 
-ERR Socket::setTimeout(uint32_t timeout, int option) {
-    if (socket == INVALID_SOCKET) return E_UNKNOWN;
+ERR Socket::setTimeout(uint32_t timeout, int option)
+{
+    if (socket == INVALID_SOCKET)
+        return E_UNKNOWN;
 
     uint32_t value = timeout * 1000;
 
-    int err = setsockopt(socket, SOL_SOCKET, option, (char *)&value, sizeof(value));
+    int err = setsockopt(socket, SOL_SOCKET, option, (char*)&value, sizeof(value));
     if (err == SOCKET_ERROR)
         return W_SET_TIMEOUT;
 
     return E_OK;
 }
-ERR Socket::init(PCSTR IP, uint16_t port, IPPROTO protocol) {
+
+ERR Socket::init(PCSTR ip_str, uint16_t port, IPPROTO protocol)
+{
     int sock_type;
 
     switch (protocol) {
@@ -35,17 +38,17 @@ ERR Socket::init(PCSTR IP, uint16_t port, IPPROTO protocol) {
 
     return E_OK;
 }
-ERR Socket::connect(PCSTR IP, uint16_t port) {
-    if (socket == INVALID_SOCKET) return E_UNKNOWN;
 
-    sockaddr_in serverAddr{
-        AF_INET,
-        htons(port)
-    };
+ERR Socket::connect(PCSTR ip_str, uint16_t port)
+{
+    if (socket == INVALID_SOCKET)
+        return E_UNKNOWN;
 
-    //socketDesc.sin_family = AF_INET;
-    //socketDesc.sin_port = htons(port);
-    inet_pton(AF_INET, IP, &(serverAddr.sin_addr.s_addr));
+    sockaddr_in serverAddr{ AF_INET, htons(port) };
+
+    // socketDesc.sin_family = AF_INET;
+    // socketDesc.sin_port = htons(port);
+    inet_pton(AF_INET, ip_str, &(serverAddr.sin_addr.s_addr));
 
     int err = ::connect(socket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
     if (err == SOCKET_ERROR) {
@@ -61,19 +64,26 @@ ERR Socket::connect(PCSTR IP, uint16_t port) {
 
     return E_OK;
 }
-ERR Socket::shutdown(int how) {
-    if (socket == INVALID_SOCKET) return E_OK;
+
+ERR Socket::shutdown(int how)
+{
+    if (socket == INVALID_SOCKET)
+        return E_OK;
 
     int err = ::shutdown(socket, how);
     if (err == SOCKET_ERROR)
         return W_SHUTDOWN;
 
-    if (status) status--;
+    if (status)
+        status--;
 
     return E_OK;
 }
-ERR Socket::close() {
-    if (socket == INVALID_SOCKET) return E_OK;
+
+ERR Socket::close()
+{
+    if (socket == INVALID_SOCKET)
+        return E_OK;
 
     int err = ::closesocket(socket);
     if (err == SOCKET_ERROR)
--- Common/Socket.h
+++ Common/Socket.h
@@ -1,30 +1,36 @@
 #pragma once
 #include <atomic>
 
-#include <winsock2.h>
 #include <Ws2tcpip.h>
+#include <winsock2.h>
 
 #include "Error.h"
 
 class Socket {
+private:
+    SOCKET socket;
+
 public:
-    Socket() {
-        socket = INVALID_SOCKET;
-        status = 0;
-    };
-    ~Socket() {
+    Socket()
+        : socket{ INVALID_SOCKET }
+        , status{ 0 }
+    {
+    }
+
+    ~Socket()
+    {
         shutdown(SD_BOTH);
         close();
-    };
-    SOCKET getSocket() { return socket; };
+    }
 
+    SOCKET getSocket() const { return socket; };
     ERR setTimeout(uint32_t timeout, int option);
-    ERR init(PCSTR IP, uint16_t port, IPPROTO protocol);
-    ERR connect(PCSTR IP, uint16_t port);
+    // PCSTR??????
+    ERR init(PCSTR ip_str, uint16_t port, IPPROTO protocol);
+    ERR connect(PCSTR ip_str, uint16_t port);
     ERR shutdown(int how);
     ERR close();
 
+    // this is wrong!
     std::atomic_uint8_t status; // Count of opened streams (read/write) of the socket
-private:
-    SOCKET socket;
 };
